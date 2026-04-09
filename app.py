904cf52f60a2154b4532c439fd4f10f6a210f390
mutea5 <bgichuru@kabarak.ac.ke>
2026-04-09 18:27:41 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
-from flask import Flask,render_template,request,redirect,url_for
-app= Flask(__name__)
-goals = []
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///goal.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def home():
-    return render_template('home.html',goals=goals)
+    goals = Goal.query.all()
+    return render_template('home.html', goals=goals)
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_goal():
     if request.method == 'POST':
-        name = request.form['name']
-        category = request.form['category']
-        goals.append({'name': name, 'category': category})
+        new_goal = Goal(name=request.form['name'], category=request.form['category'])
+        db.session.add(new_goal)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
-@app.route('/delete/<int:index>')
-def delete(index):
-    if 0 <= index < len(goals):
-        goals.pop(index)
-    return redirect(url_for('home'))
-@app.route('/edit/<int:index>', methods=['GET', 'POST'])
-def edit_goal(index):
-    if request.method == 'POST':
-        goals[index]['name'] = request.form['name']
-        goals[index]['category'] = request.form['category']
-        return redirect(url_for('home'))
-    return render_template('edit.html', goal=goals[index])
 
-if __name__=='__main__':
+@app.route('/delete/<int:id>')
+def delete_goal(id):
+    goal = Goal.query.get_or_404(id)
+    db.session.delete(goal)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_goal(id):
+    goal = Goal.query.get_or_404(id)
+
+    if request.method == 'POST':
+        goal.name = request.form['name']
+        goal.category = request.form['category']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', goal=goal)
+
+if __name__ == '__main__':
     app.run(debug=True)
 
 
